063c40d64173ae2db9f429094917c5a85eaf8a66
Rev. Johnny Healey <rev.null@gmail.com>
2010-10-19 18:11:21 -0400

    Starting work on bbking.

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(name="bbking",
+    version="0.0.1",
+    description="A Django BBCode Parser",
+    author="Rev. Johnny Healey",
+    author_email="rev.null@gmail.com",
+    packages=find_packages(),
+    package_data={
+        'djbb':['media/*','templates/*'],
+    })
+
