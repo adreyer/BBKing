b316ca1616298de9306c109adae3bff418b11726
Rev. Johnny Healey <rev.null@gmail.com>
2010-10-19 22:36:16 -0400

    adding some more bbtags.

--- bbking/bbtags/text.py
+++ bbking/bbtags/text.py
@@ -1,8 +1,17 @@
 
 from bbking import BBTag
 
-__all__ = ['BBTagItalic', ]
+__all__ = ['BBTagItalic', 'BBTagBold', 'BBTagUnderline', 'BBTagStrikethrough']
 
 class BBTagItalic(BBTag):
     tag_name = 'i'
+
+class BBTagBold(BBTag):
+    tag_name = 'b'
+
+class BBTagUnderline(BBTag):
+    tag_name = "u"
+
+class BBTagStrikethrough(BBTag):
+    tag_name = "s"
 
