ae677c4a8c754bf37d5cf8efea43270c0dc63233
Rev. Johnny Healey <rev.null@gmail.com>
2010-10-20 22:33:25 -0400

    passing another unit test.

--- bbking/parser.py
+++ bbking/parser.py
@@ -41,16 +41,23 @@ def p_content(p):
                | empty
     '''
     if len(p) == 3:
-        p[0] = p[1] + [p[2]]
+        p[0] = p[1] + p[2]
     else:
         p[0] = []
 
 def p_tagged(p):
     '''tagged : opentag content closetag'''
-    name, arg, kwargs = p[1]
-    if name != p[3]:
-        raise SyntaxError, "Unbalanced tags: [%s] [/%s]"%(name, p[3])
-    p[0] = Tagged(name, compress(p[2]), arg, **kwargs)
+    name, arg, kwargs, raw = p[1]
+    close_name, close_raw = p[3]
+    if name != close_name:
+        p[0] = [raw] + p[2] + [close_raw]
+        return
+    p[0] = [Tagged(name, compress(p[2]), arg, **kwargs)]
+
+def p_tagged_error(p):
+    '''tagged : opentag content error'''
+
+    print "p_tagged_error", list(p)
 
 def p_untagged(p):
     '''untagged : SYMBOL
@@ -61,7 +68,7 @@ def p_untagged(p):
                 | SLASH
                 | errors
     '''
-    p[0] = p[1]
+    p[0] = [p[1]]
 
 def p_errors(p):
     '''errors : LBRACKET error
@@ -126,39 +133,49 @@ def p_text_no_ws(p):
 def p_term_no_ws(p):
     ''' term_no_ws : SYMBOL
                    | MISC
+                   | SLASH
     '''
     p[0] = p[1]
 
 def p_close_tag(p):
     'closetag : LBRACKET SLASH SYMBOL RBRACKET'
-    p[0] = p[3]
+    raw = "".join(p[1:])
+    p[0] = (p[3], raw)
 
 def p_simple_tag(p):
     'opentag : LBRACKET SYMBOL RBRACKET'
-    p[0] = (p[2], None, {})
+    raw = "".join(p[1:])
+    p[0] = (p[2], None, {}, raw)
 
 def p_single_arg_tag(p):
     'opentag : LBRACKET SYMBOL EQ text RBRACKET'
-    p[0] = (p[2], compress(p[4])[0], {})
+    raw = p[1] + p[2] + p[3] + p[4][0] + p[5]
+    p[0] = (p[2], compress(p[4])[0], {}, raw)
 
 def p_multi_arg_tag(p):
     'opentag : LBRACKET SYMBOL WHITESPACE args RBRACKET'
-    p[0] = (p[2], None, p[4])
+    args, raw_args = p[4]
+    raw = p[1] + p[2] + p[3] + p[4][1] + p[5]
+    p[0] = (p[2], None, args, raw)
 
 def p_tag_args(p):
     '''args : args WHITESPACE arg
             | arg
     '''
     if len(p) == 4:
-        key,value = p[3]
-        p[0] = dict(p[1],**{ key : value})
+        key,value,raw_arg = p[3]
+        d, raw_args = p[1]
+        raw = raw_args + p[2] + raw_arg
+        p[0] = (dict(d,**{ key : value}), raw)
     else:
-        key,value = p[1]
-        p[0] = { key : value }
+        key,value,raw = p[1]
+        p[0] = ({ key : value }, raw)
 
 def p_tag_arg(p):
     'arg  : SYMBOL EQ text_no_ws'
-    p[0] = (p[1], compress(p[3])[0])
+    p[3] = compress(p[3])[0]
+    raw = "".join(p[1:])
+    p[0] = (p[1], p[3], raw)
 
 def p_error(p):
     # ignore errors for now simply don't run bbcode if it does not parse
