e38fca229e7f2f86dd4020eb1e21cb6edbea9e64
S. Cerevisiae <cerevisiae@mdc2.org>
2010-11-01 23:02:16 -0400

    forgot to add the quote module.

---
+++ bbking/bbtags/quote.py
@@ -0,0 +1,16 @@
+
+from bbking import BBTag
+
+__all__ = ['BBQuote']
+
+class BBQuote(BBTag):
+    tag_name = 'quote'
+
+    def __init__(self, contents, arg=None):
+        super(BBQuote, self).__init__(contents)
+        self.arg = arg
+
+    def update_context(self, context):
+        if self.arg:
+            context['cite'] = self.arg
+
