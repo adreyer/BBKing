6a3aed782657eb63ea0b0868fed22fd22aa2a018
Alex Dreyer <adreyer>
2010-11-13 13:51:23 -0500

    added code tag

--- bbking/bbtags/quote.py
+++ bbking/bbtags/quote.py
@@ -4,12 +4,8 @@ from bbking import BBTag
 __all__ = ['BBQuote']
 
 class BBQuote(BBTag):
-    takes_arg = True
     tag_name = 'quote'
 
-    def __init__(self, contents, arg=None):
-        super(BBQuote, self).__init__(contents)
-        self.arg = arg
 
     def update_context(self, context):
         if self.arg:
